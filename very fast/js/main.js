7bda257ecccee424b1ad42ddf9ac95e853f61866
sedoy <C:\Users\sedoy\AppData\Roaming\The Bat!>
2018-03-27 23:39:55 +0300

    6commit

--- very fast/js/main.js
+++ very fast/js/main.js
@@ -11,6 +11,15 @@ document.writeln(  week[5] + '<br>');
 document.writeln(  week[6] );
 
 
-var arr = ['33333333', '44444', '8888', '77777777', '222222', '9999', '33333'];
+/*var arr = ['33333333', '44444', '8888', '77777777', '222222', '9999', '33333'];
 const result = arr.filter(arr => arr.length > 7);
-console.log(result);
+console.log(result);*/
+
+var arr = ['33333333', '44444', '8888', '77777777', '222222', '9999', '33333'];
+function filterItems(query) {
+  return arr.filter(function(el) {
+      return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+  })
+}
+console.log(filterItems('3'));
+console.log(filterItems('7'));
