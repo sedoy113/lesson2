e2e6d93cb775f3778070b4ca073641859facc09d
sedoy <C:\Users\sedoy\AppData\Roaming\The Bat!>
2018-03-27 17:49:07 +0300

    3commit

--- light/js/main.js
+++ light/js/main.js
@@ -24,26 +24,53 @@ alert('Ваш бюджет на 1 день' + yourBudget/30);*/
 
 //Иван
 		let money 			= prompt ("Ваш бюджет?");
-		let name 				= prompt ("Название Вашего магазина?");
-
-
+		let nameShop 		= prompt ("Название Вашего магазина?");
+		let time 				= 19;
 
 	let mainList = {
 		budget: money,
-		shopName: name,
+		shopName: nameShop,
 		shopGoods: [],
 		open: false,
 		employers: {}
 	};
 		//установили шаг цикла при помощи инкремента
+		/*let i = 0;
+		for (; i < 5; i++)
+			alert( i !== 5 )*/
+
+			/*	while (i < 5) {
+			i++;
+		}*/
+		
+
 	for (let i = 0; i < 5; i++) {
 
 		let a = prompt ("Какой тип товаров будем продавать?");
-		mainList.shopGoods[i] = a;
-
+	
+		/*if-(оператор если), else-(еще) &&-(оператор и )знак сравнения, 
+		length-(ограничение параметра) колл-во знаков, !=(проверка равенства)*/
+		if ((typeof(a)) === 'string'  &&  (typeof(a)) !== null && a != '' && a.length < 50 ) {
+			mainList.shopGoods[i] = a;					
+					
+		} else  { 
+				 	alert('Введите товар!');
+			}
+		
 	}
 
+	if (time < 0) {
+		console.log('Такого не может быть');
+	} else if(time > 8 && time < 20) {
+			console.log('Время работать!');
+			} else if(time < 24) {
+				console.log('Магазин не работает');
+				} else {
+					console.log('В сутках только 24 часа!');
+				}
+
 	console.log(mainList);
+	alert('Ваш бюджет на 1 день' + money/30);
 
 
 
