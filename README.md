b6ad697e42091dd5ca2326a54402b818e53fede4
sedoy <C:\Users\sedoy\AppData\Roaming\The Bat!>
2018-03-27 20:22:17 +0300

    6commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # lesson2
-
+very fast-очень сложное
 fast	- усложненное
 light - обычное
---
+++ very fast/js/main.js
@@ -0,0 +1,12 @@
+
+var week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+
+document.writeln( '<H1>Дни недели</H1>');
+document.write(  week[0]  + '<br>');
+document.write(  week[1]  + '<br>' );
+document.writeln( '<i>'+ week[2] +'</i>'+ '<br>');
+document.writeln(  week[3] + '<br>');
+document.writeln(  week[4] + '<b>' + '<br>' );
+document.writeln(  week[5] + '<br>');
+document.writeln(  week[6] );
+
